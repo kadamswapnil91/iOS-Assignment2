997e224def6bfc66bf31b837be26c5d94dd9208f
Swapnil <kadamswapnil91@gmail.com>
2020-06-23 12:35:23 +0530

    Initial Commit

---
+++ Json post/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Json post
+//
+//  Created by Swapnil Kadam on 23/06/20.
+//  Copyright © 2020 Swapnil Kadam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
